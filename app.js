93887ad77d37f1cc3b73d9961a952526bc2aff90
Thiago Aguiar <aguiar.dev.br@gmail.com>
2023-08-23 17:38:05 -0300

    Novas funcionalidades: Roteamento do Site, Criação das postagens para a página Home, Alterações no CSS

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const port = 3000;
 const path = require('path');
 const ejs = require("ejs");
 
-const header = "Aguiar Node"
+//user datas
+const items = [];
 
 //Directory static default.
 app.use(express.static(path.join(__dirname + "/public")));
@@ -13,9 +14,33 @@ app.use(express.urlencoded({extended: true}));
 //Definindo ao express que usaremos o EJS com View
 app.set("view engine", "ejs");
 
+
+//Requisiçoes
 app.get("/", (req, res) => {
-    res.render("home", {header: header});
+    res.render("home", {items: items});
 })
+
+app.get("/about", (req, res) => {
+    res.render("about");
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact");
+})
+
+app.get("/post", (req, res) => {
+    res.render("post");
+})
+
+//Method Post
+
+app.post("/post", (req, res) => {
+    const datas = req.body;
+    console.log(datas)
+    items.push(datas);
+})
+
+
 
 app.listen(port, () => {
     console.log("Server running on port 3000");
